2be496e1f8496e43bc4a103be733b8150c2dac08
Alirezaa9 <145472044+Alirezaa9@users.noreply.github.com>
2024-06-08 01:12:54 +0000

    add score counter and display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,18 @@
-//ROCK PAPER SCISSORS
+// ROCK PAPER SCISSORS
 
 const choices = ["Rock", "Paper", "Scissors"];
 const playerDisplay = document.getElementById("playerDisplay");
 const computerDisplay = document.getElementById("computerDisplay");
-const resultDisplay = document.getElementById("resultDisplay")
+const resultDisplay = document.getElementById("resultDisplay");
+const playerScoreDisplay = document.getElementById("playerScoreDisplay");
+const computerScoreDisplay = document.getElementById("computerScoreDisplay");
+
+let playerScore = 0;
+let computerScore = 0;
 
 function playGame(playerChoice) {
     const computerChoice = choices[Math.floor(Math.random() * 3)];
-    
+
     playerDisplay.textContent = "PLAYER: " + playerChoice;
     computerDisplay.textContent = "COMPUTER: " + computerChoice;
 
@@ -19,12 +24,11 @@ function playGame(playerChoice) {
         (playerChoice === "Scissors" && computerChoice === "Paper")
     ) {
         resultDisplay.textContent = "You win!";
+        playerScore++;
+        playerScoreDisplay.textContent = playerScore;
     } else {
         resultDisplay.textContent = "Computer wins!";
+        computerScore++;
+        computerScoreDisplay.textContent = computerScore;
     }
-
-    console.log()
-
 }
-
-
